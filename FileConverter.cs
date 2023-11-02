0afb456f8bb8b19d79458e7a1990ed3b4b8a8a7a
Trygve Falch <trygve.falch@gmail.com>
2023-11-02 22:39:57 +0100

    Initial commit.

---
+++ FileConverter.cs
@@ -0,0 +1,117 @@
+﻿using PCAxis.Paxiom;
+using PCAxis.Serializers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using CommandLine;
+
+class Program
+{
+    // Command line options class
+    public class Options
+    {
+        [Option('l', "list", Required = false, HelpText = "List available serializers.")]
+        public bool List { get; set; }
+
+        [Option('i', "input", Required = false, HelpText = "Input .px file path.")]
+        public string Input { get; set; }
+
+        [Option('o', "output", Required = false, HelpText = "Output file path.")]
+        public string Output { get; set; }
+
+        [Option('s', "serializer", Required = false, HelpText = "Serializer type.")]
+        public string Serializer { get; set; }
+    }
+
+    private static class SerializerFactory
+    {
+        public static IPXModelStreamSerializer CreateSerializer(string serializerType)
+        {
+            var serializerTypes = GetAvailableSerializers();
+            if (!serializerTypes.ContainsKey(serializerType.ToLower()))
+            {
+                throw new ArgumentException($"Unsupported format: {serializerType}");
+            }
+
+            var serializerInstance = (IPXModelStreamSerializer)Activator.CreateInstance(serializerTypes[serializerType.ToLower()]);
+
+            if (serializerInstance is XlsxSerializer xlsxSerializer)
+            {
+                xlsxSerializer.DoubleColumn = DoubleColumnType.NoDoubleColumns;
+                xlsxSerializer.InformationLevel = InformationLevelType.AllInformation;
+            }
+
+            return serializerInstance;
+        }
+    }
+
+    private static Dictionary<string, Type> GetAvailableSerializers()
+    {
+        Assembly assembly = typeof(PCAxis.Serializers.Html5TableSerializer).Assembly;
+
+        var serializerTypes = assembly.GetTypes()
+                                      .Where(t => t.GetInterfaces().Contains(typeof(IPXModelStreamSerializer)))
+                                      .ToDictionary(t => t.Name.Replace("Serializer", "").ToLower(), t => t);
+
+        return serializerTypes;
+    }
+
+    private static void SerializeModel(string serializerType, string inputPath, string outputPath)
+    {
+        var builder = new PXFileBuilder();
+        builder.SetPath(new Uri(inputPath, UriKind.Relative).ToString());
+        builder.BuildForSelection();
+        var selection = Selection.SelectAll(builder.Model.Meta);
+        builder.BuildForPresentation(selection);
+        var model = builder.Model;
+
+        using (FileStream stream = new FileStream(new Uri(outputPath, UriKind.Relative).ToString(), FileMode.Create))
+        {
+            var serializerInstance = SerializerFactory.CreateSerializer(serializerType);
+            try
+            {
+                serializerInstance!.Serialize(model, stream);
+                Console.WriteLine($"Serialization to {serializerType} completed.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args)
+              .WithParsed<Options>(opts => HandleOptions(opts))
+              .WithNotParsed(errors => HandleParseError(errors));
+    }
+
+    private static void HandleOptions(Options opts)
+    {
+        if (opts.List)
+        {
+            Console.WriteLine("Available serializers:");
+            foreach (var type in GetAvailableSerializers().Keys)
+            {
+                Console.WriteLine(type);
+            }
+            return;
+        }
+
+        if (string.IsNullOrEmpty(opts.Input) || string.IsNullOrEmpty(opts.Output) || string.IsNullOrEmpty(opts.Serializer))
+        {
+            Console.WriteLine("Please provide valid input, output paths, and serializer type using -i, -o, and -s respectively.");
+            return;
+        }
+
+        SerializeModel(opts.Serializer, opts.Input, opts.Output);
+    }
+
+    private static void HandleParseError(IEnumerable<Error> errors)
+    {
+        Console.WriteLine("Error parsing command line arguments.");
+    }
+}
---
+++ README.md
@@ -0,0 +1,69 @@
+# PxTools.FileConverter
+
+`PxTools.FileConverter` is a simple console application designed for the conversion of `.px` files into various serialization formats. Built on top of the PCAxis framework, it offers the transformation of statistical data with ease and precision.
+
+## Features
+
+- **Dynamic Serialization**: Convert `.px` files into various formats including JSON, Excel, Parquet, and more.
+- **List Available Serializers**: Quickly view the supported serialization formats.
+- **Command Line Driven**: Intuitive command line arguments for easy operations.
+
+## Prerequisites
+
+- .NET Core 3.1 or later
+
+## Dependencies
+
+- `CommandLineParser` version `2.9.1`
+- `PcAxis.Core` version `1.2.3`
+- `PcAxis.Serializers` version `1.1.2`
+
+## Installation
+
+1. Clone the repository:
+   ```
+   git clone https://github.com/trygu/PxTools.FileConverter.git
+   ```
+2. Navigate to the project directory:
+   ```
+   cd PxTools.FileConverter
+   ```
+3. Restore the NuGet packages:
+   ```
+   dotnet restore
+   ```
+
+## Usage
+
+**List Available Serializers**:
+```
+app -l
+```
+or
+```
+app --list
+```
+
+**Convert `.px` File**:
+```
+app -i input.px -o output.json -s json
+```
+or
+```
+app --input input.px --output output.json --serializer json
+```
+
+## Parameters
+
+- `-l` or `--list`: Lists the available serializers.
+- `-i` or `--input`: Specifies the path to the input `.px` file.
+- `-o` or `--output`: Specifies the path for the output serialized file.
+- `-s` or `--serializer`: Specifies the type of serializer to use (e.g., `json`).
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
